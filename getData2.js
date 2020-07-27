93c60eff7a7440a0e4954cbccc218407740c8838
lnsqj123 <holmane33@gmail.com>
2020-07-28 02:18:25 +0900

    bonus

--- getData2.js
+++ getData2.js
@@ -5,7 +5,7 @@ const log = console.log;
 
 const getData = async () => {
     try {
-        return await axios.get("https://www.seoul.go.kr/coronaV/coronaStatus.do");
+        return await axios.get("https://news.seoul.go.kr/welfare/archives/513105");
     } catch (error) {
         console.error(error);
     }
@@ -15,17 +15,24 @@ getData()
   .then(html => {
     let ulList = [];
     const $ = cheerio.load(html.data);
-    const $bodyList = $("table.table-path tbody").children(".show");
+    const $bodyList = $("table.route-datatable tbody").children("tr.show");
     
     $bodyList.each(function(i, elem) {
-        ulList[i] = {
-            dong: $(this).find("td").text(),
-            place: $(this).find("td").next().text(),
-            ads: $(this).find("td").next().next().text(),
-            date: $(this).find("td").next().next().next().text(),
-            check: $(this).find("td").next().next().next().next().text(),
-            bigo: $(this).find("td.aLeft").text()
-        };
+        if ($(this).find("p") == "") {
+            ulList[i] = {
+                dong: $(this).find("td").text(),
+                place: $(this).find("td").next().text(),
+                ads: $(this).find("td").next().next().text(),
+                date: $(this).find("td").next().next().next().text(),
+                check: $(this).find("td").next().next().next().next().text(),
+                bigo: $(this).find("td.aLeft").text()
+            };
+        }
+        else {
+            ulList[i] = {
+                Text: $(this).find("p").text()
+            }
+        }
         log(ulList[i])
     });
 
